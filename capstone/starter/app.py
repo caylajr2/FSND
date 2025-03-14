a12447d9d9a4d8a6f9512c8f84ae825c61a7e492
caylajr2 <caylajr2@illinois.edu>
2025-03-13 22:05:36 -0500

    create initial models

--- capstone/starter/app.py
+++ capstone/starter/app.py
@@ -3,18 +3,115 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
+from models import setup_db, Item, db
 
-  return app
+
+# pagination modified from trivia_api
+ITEMS_PER_PAGE = 20
+
+def paginate_items(request, selection):
+    page = request.args.get("page", 1, type=int)
+    start = (page - 1) * ITEMS_PER_PAGE
+    end = start + ITEMS_PER_PAGE
+
+    questions = [question.format() for question in selection]
+    current_questions = questions[start:end]
+
+    return current_questions
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__)
+
+    if test_config is None:
+        setup_db(app)
+    else:
+        database_path = test_config.get('SQLALCHEMY_DATABASE_URI')
+        setup_db(app, database_path=database_path)
+
+    """
+    @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
+    """
+    with app.app_context():
+        db.create_all()
+
+    # From trivia_api code
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
+        )
+        return response
+
+
+    """
+    Get Items
+    """
+    @app.route('/items')
+    def get_items():
+        try:
+            selection = Item.query.all()
+            return "hello worlde"
+            formatted_items = paginate_items(selection)
+            return jsonify({
+                'items': formatted_items
+            }), 200
+        except BaseException:
+            abort(404, description="get_items")
+
+
+    """
+    Error handlers
+    """
+    # Modified from trivia_api code
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found: " + error.description
+        }), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable: " + error.description
+        }), 422
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "bad request: " + error.description
+        }), 400
+
+    @app.errorhandler(500)
+    def server_error():
+        return jsonify({
+            "success": False,
+            "error": 500,
+            "message": "server error"
+        }), 500
+
+    return app
+
+
+
+
+# def create_app(test_config=None):
+#   # create and configure the app
+#   app = Flask(__name__)
+#   CORS(app)
+
+#   return app
 
 app = create_app()
-
-@app.route("/")
-def home():
-    return "Hello, world!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
--- capstone/starter/models.py
+++ capstone/starter/models.py
@@ -1,1 +1,122 @@
+from sqlalchemy import Column, String, Integer, Float
+from flask_sqlalchemy import SQLAlchemy
 
+database_name = "earring_shoppe"
+database_user = "student"
+database_password = "student"
+database_host = 'localhost:5432'
+database_path = f'postgresql://{database_user}:{database_password}@{database_host}/{database_name}'
+
+db = SQLAlchemy()
+
+"""
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+"""
+def setup_db(app, database_path=database_path):
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_path
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
+# ----------------------------------------------------------------------------#
+# Models.
+# ----------------------------------------------------------------------------#
+
+'''Item'''
+class Item(db.Model):
+    __tablename__ = 'items'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    image = Column(String, nullable=False)
+    cost = Column(Float, nullable=False)
+
+    def __init__(self, name, description, image, cost):
+        self.name = name
+        self.description = description
+        self.image = image
+        self.cost = cost
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'image': self.image,
+            'cost': self.cost
+        }
+
+'''Customer'''
+
+class Customer(db.Model):
+    __tablename__ = 'customers'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    image = Column(String, nullable=False)
+    cart_id = Column(Integer, nullable=False)
+
+    def __init__(self, name, image, cart_id):
+        self.name = name
+        self.image = image
+        self.cart_id = cart_id
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image': self.image
+        }
+
+'''Cart'''
+class Cart(db.Model):
+    __tablename__ = 'carts'
+
+    id = Column(Integer, primary_key=True)
+    customer_id = Column(Integer, nullable=False)
+    item_ids = Column((db.ARRAY(db.Integer)))
+
+    def __init__(self, customer_id, item_ids):
+        self.customer_id = customer_id
+        self.item_ids = item_ids
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'customer_id': self.customer_id,
+            'item_ids': self.item_ids
+        }
